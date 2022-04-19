614b7ce2799a9323f337805dd5c1a5bb46f74261
amat <saeram00.nova@gmail.com>
2022-04-19 14:59:27 -0400

    Final version
    Ready for delivery and evaluation.

--- family/models.py
+++ family/models.py
@@ -5,7 +5,13 @@ class Relative(models.Model):
     name = models.CharField(max_length=40)
     age = models.IntegerField()
     birth = models.DateField()
+    job = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"Name: {self.name}. Age: {self.age}. Date of Birth: {self.birth}"
+        return f"""
+                Nombre: {self.name}.
+                Edad: {self.age}.
+                Fecha de Nacimiento: {self.birth}.
+                Ocupación: {self.job}.
+                """
 
--- family/views.py
+++ family/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from .models import Relative
 
 
-rel1 = Relative(name="Robert House", age=38, birth=(1984, 12, 22))
-rel2 = Relative(name="Maria Lockhart", age=22, birth=(2000, 4, 7))
-rel3 = Relative(name="Loise Arzulo", age=28, birth=(1994, 7, 15))
-
-relatives = (rel1, rel2, rel3)
-
 fam_context = {
     'title': 'Family',
-    'relatives': relatives
+    'relatives': Relative.objects.all()
 }
 
 def family(request):
